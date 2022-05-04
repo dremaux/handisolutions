31e0e675b3789e4757225c62d4d81fae410ee163
dremaux <ldremaux@btslivh.eu>
2022-05-04 16:29:56 +0200

    fusion handi+symfony

---
+++ templates/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>ADV</title>
+    <link rel="stylesheet" href="./indexe.css">
+</head>
+
+<header>
+    <div class="image2">
+        <img src="images/bander.PNG" alt="banniere" width="650" height="200" />
+    </div>
+</header>
+
+<body>
+
+<h2>Sensibilité</h2>
+<input name="range-left" id="range-left" list="tickmarks" />
+
+<datalist id="tickmarks">
+  <option value="512"  label="1">
+  <option value="1024" label="1">
+  <option value="2048" label="1">
+  <option value="4096" label="1">
+  <option value="8192" label="1">
+</datalist>
+
+    <div class="image1">
+        <img src="images/logo1.3.PNG" alt="logo" width="350" height="210" />
+    </div>
+    <!-- partial:index.partial.html -->
+    <div id="circle3"></div>
+    <div id="circle"></div>
+    <div id="circle2"></div>
+
+    <!-- partial -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="./script.js"></script>
+    <script src="./script3.js"></script>
+
+</body>
+
+
+
+
+
+
+
+
+
+
